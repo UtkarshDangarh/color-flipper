3c9681167e36537918714abd8c2900d1e3e395e4
UtkarshDangarh <utkarsh.dangarh17@gmail.com>
2021-01-21 07:39:53 +0530

    Final Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+btn.addEventListener('click', () => {
+    // Random number between 0-3
+    const randomNumber = () => {
+        return Math.floor(Math.random() * colors.length);
+    };
+    document.body.style.backgroundColor = colors[randomNumber()];
+    color.textContent = colors[randomNumber()];
+})
